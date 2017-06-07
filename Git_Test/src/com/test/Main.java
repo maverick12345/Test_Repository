89618e456bf2ef686b22bea286674ec1b5a41474
vishnu <>
2017-06-07 12:41:06 +0530

    change

--- Git_Test/src/com/test/Main.java
+++ Git_Test/src/com/test/Main.java
@@ -5,7 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git test 1");
-		System.out.println("Git test 1");
 		
 	}
 

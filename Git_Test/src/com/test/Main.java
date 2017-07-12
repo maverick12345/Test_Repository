2b0ab1472fa4091be84c545c95469a0e24472bf5
vishnu <>
2017-07-12 20:19:47 +0530

    commit

--- Git_Test/src/com/test/Main.java
+++ Git_Test/src/com/test/Main.java
@@ -5,10 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Git test 1");
-		System.out.println("Git test 2");
-		System.out.println("Git test 3");
-		
-		
 	}
 
 }
